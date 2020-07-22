59ee81cb8024934ac9bb80bb8f3a8545849a37cc
dukim <dukim@bnpinnovation.com>
2020-07-22 15:08:11 +0900

    grpc-gw url 수정

--- build/gen/bnpinnovation.com/marine/marine.pb.gw.go
+++ build/gen/bnpinnovation.com/marine/marine.pb.gw.go
@@ -517,7 +517,7 @@ func RegisterProjectServiceHandlerClient(ctx context.Context, mux *runtime.Serve
 }
 
 var (
-	pattern_ProjectService_Summary_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3}, []string{"dropship", "api", "v1", "status"}, "", runtime.AssumeColonVerbOpt(true)))
+	pattern_ProjectService_Summary_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3}, []string{"dropship", "api", "v1", "summary"}, "", runtime.AssumeColonVerbOpt(true)))
 
 	pattern_ProjectService_Status_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 1, 0, 4, 1, 5, 3, 2, 4}, []string{"dropship", "api", "v1", "project", "status"}, "", runtime.AssumeColonVerbOpt(true)))
 
