90e6f82e413c08a58de988da9c9b8059ae45692d
dukim <dukim@bnpinnovation.com>
2020-08-05 15:22:43 +0900

    static server authentication

--- marine/static/staticServer.go
+++ marine/static/staticServer.go
@@ -4,20 +4,40 @@ import (
 	"log"
 	"net/http"
 	"os"
+	auth "github.com/abbot/go-http-auth"
 )
 
 var home, workingDir string
-
+var authenticator auth.BasicAuth
 
 func init() {
 	home, _ = os.UserHomeDir()
 	workingDir = home + "/workspace/dropship"
+	authenticator = auth.BasicAuth{Realm: "localhost", Secrets: Secret}
+}
+
+func Secret(user, realm string) string {
+
+	if user == "john" {
+		// password is "hello"
+		return "$1$dlPL2MqE$oQmn16q49SqdmhenQuNgs1"
+	}
+	return ""
+}
+
+func handleFileServer() http.HandlerFunc {
+	fs := http.FileServer(http.Dir(workingDir+"/static"))
+	handler := fs.ServeHTTP
+	return func(w http.ResponseWriter, req *http.Request) {
+		log.Println(req.URL)
+		handler(w, req)
+	}
 }
 
 func Run() error {
 	log.Println("static server serve, http://localhost:3000")
-	fs := http.FileServer(http.Dir(workingDir+"/static"))
-	http.Handle("/", fs)
+	http.HandleFunc("/static/", auth.JustCheck(&authenticator, handleFileServer()))
+
 	err := http.ListenAndServe(":3000", nil)
 	log.Println("static server terminate")
 	return err
